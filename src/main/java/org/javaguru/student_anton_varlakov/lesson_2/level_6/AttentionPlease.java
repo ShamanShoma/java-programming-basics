a189548e94545980915f7063aa8544079cf2ba72
ShamanShoma <shamanshoma@icloud.com>
2025-09-13 18:01:44 +0500

    Level_6 solutions

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_2/level_6/AttentionPlease.java
@@ -0,0 +1,7 @@
+package org.javaguru.student_anton_varlakov.lesson_2.level_6;
+
+class AttentionPlease {
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+}
