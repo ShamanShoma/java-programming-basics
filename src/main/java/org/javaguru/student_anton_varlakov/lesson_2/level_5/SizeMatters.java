5fbfe1ce887fe2f16e007559fc13cccc5b7e8582
ShamanShoma <shamanshoma@icloud.com>
2025-09-13 17:45:36 +0500

    Level_5 solutions

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_2/level_5/SizeMatters.java
@@ -0,0 +1,8 @@
+package org.javaguru.student_anton_varlakov.lesson_2.level_5;
+
+class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
