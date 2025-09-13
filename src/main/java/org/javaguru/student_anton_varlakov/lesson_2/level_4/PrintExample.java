a528d761783f9b604767acbefc0adbe0ab318eed
ShamanShoma <shamanshoma@icloud.com>
2025-09-13 17:41:07 +0500

    Level_4 solutions

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_2/level_4/PrintExample.java
@@ -0,0 +1,16 @@
+package org.javaguru.student_anton_varlakov.lesson_2.level_4;
+
+import java.util.Scanner;
+
+public class PrintExample {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите ваше имя: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Привет, " + name + "! ");
+        System.out.print("Добро пожаловать в мир Java!");
+    }
+}
