9beea6601945679020133dae0bfc1cb809b49b6e
ShamanShoma <shamanshoma@icloud.com>
2025-09-13 16:41:28 +0500

    Level_2 solutions

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_2/level_2/HelloUser.java
@@ -0,0 +1,16 @@
+package org.javaguru.student_anton_varlakov.lesson_2.level_2;
+
+import java.util.Scanner;
+
+public class HelloUser {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите ваше имя: ");
+        String name = sc.nextLine();
+
+        System.out.println("Привет, " + name);
+
+    }
+}
---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_2/level_2/LoginService.java
@@ -0,0 +1,9 @@
+package org.javaguru.student_anton_varlakov.lesson_2.level_2;
+
+class LoginService {
+
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+
+}
