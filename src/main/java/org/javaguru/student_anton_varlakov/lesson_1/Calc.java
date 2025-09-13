e81bb197b4190b62ccc550fb3ef297d77e4e8f3f
ShamanShoma <shamanshoma@icloud.com>
2025-09-13 16:21:04 +0500

    Book and Calc example

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_1/Calc.java
@@ -0,0 +1,9 @@
+package org.javaguru.student_anton_varlakov.lesson_1;
+
+public class Calc {
+    public static void main(String[] args) {
+        System.out.println(74 + 36);
+        System.out.println(50 / 3);
+        System.out.println(50 * 3);
+    }
+}
