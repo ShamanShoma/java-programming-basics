62d08f97f6a693f2a7a6825a61060803282f7954
ShamanShoma <shamanshoma@icloud.com>
2025-09-13 17:26:45 +0500

    Level_3 solutions

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_2/level_3/ArithmeticMean.java
@@ -0,0 +1,22 @@
+package org.javaguru.student_anton_varlakov.lesson_2.level_3;
+
+import java.util.Scanner;
+
+public class ArithmeticMean {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Please enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Please enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        double arithmeticMean = (double) (num1 + num2 + num3 ) / 3;
+        System.out.println("Arithmetic mean of numbers: " + arithmeticMean);
+    }
+}
---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_2/level_3/PerimeterAreaOfCircle.java
@@ -0,0 +1,20 @@
+package org.javaguru.student_anton_varlakov.lesson_2.level_3;
+
+import java.util.Scanner;
+
+public class PerimeterAreaOfCircle {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please enter the radius of the circle: ");
+        double radius = sc.nextDouble();
+
+        double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * (radius * radius);
+
+        System.out.println("Perimeter is: " + perimeter);
+        System.out.println("Area is: " + area);
+
+    }
+}
