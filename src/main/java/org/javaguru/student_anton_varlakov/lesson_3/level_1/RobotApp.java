e59b47fe2d21f2d83e99d75e5a67f01292e72694
ShamanShoma <shamanshoma@icloud.com>
2025-09-15 14:47:24 +0500

    Lesson_3 level_1 task_2

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_3/level_1/RobotApp.java
@@ -0,0 +1,10 @@
+package org.javaguru.student_anton_varlakov.lesson_3.level_1;
+
+public class RobotApp {
+    public static void main(String[] args) {
+
+        Robot name1 = new Robot("Android");
+        Robot name2 = new Robot("IOS");
+
+    }
+}
