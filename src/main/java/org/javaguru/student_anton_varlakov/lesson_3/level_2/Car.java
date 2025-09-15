549c6803b3889e0e230076d1cff8be98eee83bb8
ShamanShoma <shamanshoma@icloud.com>
2025-09-15 15:27:52 +0500

    Lesson_3 level_2 task_1

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_3/level_2/Car.java
@@ -0,0 +1,15 @@
+package org.javaguru.student_anton_varlakov.lesson_3.level_2;
+
+public class Car {
+
+    private String brand;
+    private String color;
+    private String price;
+
+    public Car(String brand, String color, String price) {
+        this.brand = brand;
+        this.color = color;
+        this.price = price;
+    }
+
+}
