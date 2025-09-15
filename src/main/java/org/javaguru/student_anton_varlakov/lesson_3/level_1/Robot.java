9fd280f16001eae88c46046bd3a7da6b552f13c6
ShamanShoma <shamanshoma@icloud.com>
2025-09-15 14:31:06 +0500

    Lesson_3 level_1 task_1

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_3/level_1/Robot.java
@@ -0,0 +1,11 @@
+package org.javaguru.student_anton_varlakov.lesson_3.level_1;
+
+public class Robot {
+
+    private String name;
+
+    public Robot(String name) {
+        this.name = name;
+    }
+
+}
