e9f5fc78519c9023e21809278be451835f279fd1
ShamanShoma <shamanshoma@icloud.com>
2025-09-17 12:54:32 +0500

    Lesson_3 level_3 task_1

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_3/level_3/Book.java
@@ -0,0 +1,19 @@
+package org.javaguru.student_anton_varlakov.lesson_3.level_3;
+
+public class Book {
+
+    private String name;
+    private String author;
+    private int year;
+    private Chapter chapter1;
+    private Chapter chapter2;
+
+    public Book(String name, String author, int year, Chapter chapter1, Chapter chapter2) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+        this.chapter1 = chapter1;
+        this.chapter2 = chapter2;
+    }
+
+}
---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_3/level_3/Chapter.java
@@ -0,0 +1,13 @@
+package org.javaguru.student_anton_varlakov.lesson_3.level_3;
+
+public class Chapter {
+
+    private String name;
+    private String textChapter;
+
+    public Chapter(String name, String textChapter) {
+        this.name = name;
+        this.textChapter = textChapter;
+    }
+
+}
