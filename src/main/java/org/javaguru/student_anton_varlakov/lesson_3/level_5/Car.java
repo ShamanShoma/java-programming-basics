97fdf2585b7adfff4ab87b33f508b8812b8754c5
ShamanShoma <shamanshoma@icloud.com>
2025-09-19 16:25:31 +0500

    Lesson_3 level_5 task_3

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_3/level_5/Car.java
@@ -0,0 +1,15 @@
+package org.javaguru.student_anton_varlakov.lesson_3.level_5;
+
+class Car {
+
+    private String model;
+
+    public Car(String newModel) {
+        this.model = newModel;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+}
---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_3/level_5/CarDemo.java
@@ -0,0 +1,11 @@
+package org.javaguru.student_anton_varlakov.lesson_3.level_5;
+
+class CarDemo {
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+    }
+
+}
