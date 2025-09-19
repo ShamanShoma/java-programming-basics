7e3bd5d70b05a31bbd82febb5b9d47b26e552b2c
ShamanShoma <shamanshoma@icloud.com>
2025-09-19 16:22:16 +0500

    Lesson_3 level_5 task_2

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_3/level_5/Phone.java
@@ -0,0 +1,15 @@
+package org.javaguru.student_anton_varlakov.lesson_3.level_5;
+
+class Phone {
+
+    private String model;
+
+    public Phone(String model) {
+        this.model = model;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+}
---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_3/level_5/PhoneDemo.java
@@ -0,0 +1,11 @@
+package org.javaguru.student_anton_varlakov.lesson_3.level_5;
+
+class PhoneDemo {
+
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+
+}
