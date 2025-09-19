0ba00bbc6b2d3283b17f1c8714f68d8d55e15739
ShamanShoma <shamanshoma@icloud.com>
2025-09-19 16:18:35 +0500

    Lesson_3 level_5 task_1

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_3/level_5/Book.java
@@ -0,0 +1,15 @@
+package org.javaguru.student_anton_varlakov.lesson_3.level_5;
+
+class Book {
+
+    private String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+}
---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_3/level_5/BookDemo.java
@@ -0,0 +1,11 @@
+package org.javaguru.student_anton_varlakov.lesson_3.level_5;
+
+class BookDemo {
+
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+    }
+
+}
