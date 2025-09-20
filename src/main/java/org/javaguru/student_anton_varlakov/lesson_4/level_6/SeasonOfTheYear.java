97a961d4f29c4bd2c8755554e5e6812c845416c0
ShamanShoma <shamanshoma@icloud.com>
2025-09-20 12:54:06 +0500

    Refactor: Lesson_4 level_6 task_1

--- src/main/java/org/javaguru/student_anton_varlakov/lesson_4/level_6/SeasonOfTheYear.java
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_4/level_6/SeasonOfTheYear.java
@@ -11,19 +11,20 @@ class SeasonOfTheYear {
         int monthNum = scanner.nextInt();
 
         switch (monthNum) {
-            case 1 -> System.out.println("This is winter!");
-            case 2 -> System.out.println("This is winter!");
-            case 3 -> System.out.println("This is spring");
-            case 4 -> System.out.println("This is spring!");
-            case 5 -> System.out.println("This is spring!");
-            case 6 -> System.out.println("This is summer!");
-            case 7 -> System.out.println("This is summer!");
-            case 8 -> System.out.println("This is summer!");
-            case 9 -> System.out.println("This is autumn!");
-            case 10 -> System.out.println("This is autumn!");
-            case 11 -> System.out.println("This is autumn!");
-            case 12 -> System.out.println("This is winter!");
-            default -> System.out.println("Incorrect number!");
+            case 1: case 2: case 12:
+                System.out.println("This is winter!");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("This is spring");
+                break;
+            case 6: case 7: case 8:
+                System.out.println("This is summer!");
+                break;
+            case 9: case 10: case 11:
+                System.out.println("This is autumn!");
+                break;
+            default:
+                System.out.println("Incorrect number!");
         }
     }
 
