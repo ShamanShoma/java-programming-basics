0904e289e0ab32c15463211b3d268e03353218da
ShamanShoma <shamanshoma@icloud.com>
2025-09-20 08:17:15 +0500

    Lesson_4 level_2 task_1

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_4/level_2/LargerNumber.java
@@ -0,0 +1,22 @@
+package org.javaguru.student_anton_varlakov.lesson_4.level_2;
+
+import java.util.Scanner;
+
+class LargerNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        if (num1 > num2) {
+            System.out.println(num1 + " is greater than " + num2);
+        } else {
+            System.out.println(num2 + " is greater than " + num1);
+        }
+    }
+
+}
