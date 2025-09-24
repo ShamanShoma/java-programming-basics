e2f61cbc818b06750e6565ea4ce82674b3b2cfb3
ShamanShoma <shamanshoma@icloud.com>
2025-09-24 10:24:05 +0500

    Lesson_5 level_1 task_2

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_5/level_1/task_2/WaveColors.java
@@ -0,0 +1,23 @@
+package org.javaguru.student_anton_varlakov.lesson_5.level_1.task_2;
+
+class WaveColors {
+
+    String color(int number){
+        if ((number >= 380) && (number <= 449)) {
+            return "Violet";
+        } else if ((number >= 450) && (number <= 494)) {
+            return "Blue";
+        } else if ((number >= 495) && (number <= 569)) {
+            return "Green";
+        } else if ((number >= 570) && (number <= 589)) {
+            return "Yelow";
+        } else if ((number >= 590) && (number <= 619)) {
+            return "Orange";
+        } else if ((number >= 620) && (number <= 750)) {
+            return "Red";
+        } else {
+            return "Invisible Light";
+        }
+    }
+
+}
---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_5/level_1/task_2/WaveColorsApp.java
@@ -0,0 +1,17 @@
+package org.javaguru.student_anton_varlakov.lesson_5.level_1.task_2;
+
+class WaveColorsApp {
+
+    public static void main(String[] args) {
+        WaveColors waveColors = new WaveColors();
+
+        System.out.println(waveColors.color(380));
+        System.out.println(waveColors.color(494));
+        System.out.println(waveColors.color(502));
+        System.out.println(waveColors.color(571));
+        System.out.println(waveColors.color(618));
+        System.out.println(waveColors.color(750));
+        System.out.println(waveColors.color(2));
+    }
+
+}
