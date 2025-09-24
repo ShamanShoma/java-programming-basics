597db2305273551d0c4117f19fa7707a4de4a9ad
ShamanShoma <shamanshoma@icloud.com>
2025-09-24 11:44:09 +0500

    Lesson_5 level_4 task_1

---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_5/level_4/AreaAndPerimeterCircle.java
@@ -0,0 +1,13 @@
+package org.javaguru.student_anton_varlakov.lesson_5.level_4;
+
+class AreaAndPerimeterCircle {
+
+    double calculateArea(double radius) {
+        return Math.PI * (radius * radius);
+    }
+
+    double calculatePerimeter(double radius) {
+        return 2 * Math.PI * radius;
+    }
+
+}
---
+++ src/main/java/org/javaguru/student_anton_varlakov/lesson_5/level_4/AreaAndPerimeterCircleApp.java
@@ -0,0 +1,12 @@
+package org.javaguru.student_anton_varlakov.lesson_5.level_4;
+
+class AreaAndPerimeterCircleApp {
+
+    public static void main(String[] args) {
+        AreaAndPerimeterCircle circleServis = new AreaAndPerimeterCircle();
+
+        System.out.println(circleServis.calculateArea(8.4));
+        System.out.println(circleServis.calculatePerimeter(8));
+    }
+
+}
